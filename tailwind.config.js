4a1d073ff25c9c5a724457874c4d5ff4f3712beb
Javohir <narimonovjavohir2009@gmail.com>
2026-02-26 11:02:18 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primery: '#FF8A00',
+        secodary: '#3FA72F',
+        thirstage: '#311F09',
+
+      }
+    },
+  },
+  plugins: [],
+}
+
